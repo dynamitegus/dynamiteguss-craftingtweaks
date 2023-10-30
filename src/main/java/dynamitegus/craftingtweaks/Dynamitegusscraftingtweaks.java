80f9152f6d0ab5c2d564518ced51a1bef4228fed
dynamitegus <69379880+dynamitegus@users.noreply.github.com>
2023-10-31 10:35:13 +1100

    adding a texture

--- src/main/java/dynamitegus/craftingtweaks/Dynamitegusscraftingtweaks.java
+++ src/main/java/dynamitegus/craftingtweaks/Dynamitegusscraftingtweaks.java
@@ -1,6 +1,10 @@
 package dynamitegus.craftingtweaks;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +14,11 @@ public class Dynamitegusscraftingtweaks implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("dynamiteguss-craftingtweaks");
+
+    public static final Item CUSTOM_ITEM =
+      Registry.register(Registry.ITEM, new Identifier("dynamiteguss-craftingtweaks", "rescourceblob"),
+        new Item(new FabricItemSettings()));
+ 
 
 	@Override
 	public void onInitialize() {
